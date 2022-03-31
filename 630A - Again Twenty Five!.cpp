6acf748aeab546538a3010dee85c2c8b7a430551
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-03-31 18:37:21 +0530

    Create 630A - Again Twenty Five!.cpp
    Output Status:
    
    By Rajakunalpandit, contest: Experimental Educational Round: VolBIT Formulas Blitz, problem: (A) Again Twenty Five!, Accepted
    
    The HR manager was disappointed again. The last applicant failed the interview the same way as 24 previous ones. "Do I give such a hard task?" — the HR manager thought. "Just raise number 5 to the power of n and get last two digits of the number. Yes, of course, n can be rather big, and one cannot find the power using a calculator, but we need people who are able to think, not just follow the instructions."
    
    Could you pass the interview in the machine vision company in IT City?
    
    Input
    The only line of the input contains a single integer n (2 ≤ n ≤ 2·1018) — the power in which you need to raise number 5.
    
    Output
    Output the last two digits of 5n without spaces between them.
    
    Examples
    inputCopy
    2
    outputCopy
    25

---
+++ 630A - Again Twenty Five!.cpp
@@ -0,0 +1,33 @@
+Output Status:
+
+By Rajakunalpandit, contest: Experimental Educational Round: VolBIT Formulas Blitz, problem: (A) Again Twenty Five!, Accepted
+
+
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <cctype>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+
+int32_t main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    ll n;
+    cin >> n;
+    
+    cout<< 25 << endl;
+
+    
+	return 0;
+}
