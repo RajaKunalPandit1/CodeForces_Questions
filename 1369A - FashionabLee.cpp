c6f8cbd7fff43134586bb08e0150cf3d74fc5c54
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-04-29 19:47:16 +0530

    Create 1369A - FashionabLee.cpp
    Output Status: 
    
    By Rajakunalpandit, contest: Codeforces Round #652 (Div. 2), problem: (A) FashionabLee, Accepted
    
    Lee is going to fashionably decorate his house for a party, using some regular convex polygons...
    
    Lee thinks a regular n-sided (convex) polygon is beautiful if and only if he can rotate it in such a way that at least one of its edges is parallel to the OX-axis and at least one of its edges is parallel to the OY-axis at the same time.
    
    Recall that a regular n-sided polygon is a convex polygon with n vertices such that all the edges and angles are equal.
    
    Now he is shopping: the market has t regular polygons. For each of them print YES if it is beautiful and NO otherwise.
    
    Input
    The first line contains a single integer t (1≤t≤104) — the number of polygons in the market.
    
    Each of the next t lines contains a single integer ni (3≤ni≤109): it means that the i-th polygon is a regular ni-sided polygon.
    
    Output
    For each polygon, print YES if it's beautiful or NO otherwise (case insensitive).
    
    Example
    inputCopy
    4
    3
    4
    12
    1000000000
    outputCopy
    NO
    YES
    YES
    YES
    Note
    In the example, there are 4 polygons in the market. It's easy to see that an equilateral triangle (a regular 3-sided polygon) is not beautiful, a square (a regular 4-sided polygon) is beautiful and a regular 12-sided polygon (is shown below) is beautiful as well.

---
+++ 1369A - FashionabLee.cpp
@@ -0,0 +1,46 @@
+Output Status: 
+
+By Rajakunalpandit, contest: Codeforces Round #652 (Div. 2), problem: (A) FashionabLee, Accepted
+
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <cctype>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+
+int32_t main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    ll t;
+    cin>> t;
+    
+    while(t--){
+        
+        ll n;
+        cin>> n;
+        
+        if(n%4 == 0){
+            cout<< "YES" << endl;
+        }else{
+            cout<< "NO" << endl;
+        }
+        
+    }
+
+    
+	return 0;
+}
+
+
+
