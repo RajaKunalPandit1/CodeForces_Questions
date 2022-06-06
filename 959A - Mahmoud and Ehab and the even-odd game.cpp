a72fa37b5ff24d24aeb84640581d31925674f096
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-06-06 15:24:19 +0530

    Create 959A - Mahmoud and Ehab and the even-odd game.cpp
    Output Status: 
    
    By Rajakunalpandit, contest: Codeforces Round #473 (Div. 2), problem: (A) Mahmoud and Ehab and the even-odd game, Accepted
    
    Mahmoud and Ehab play a game called the even-odd game. Ehab chooses his favorite integer n and then they take turns, starting from Mahmoud. In each player's turn, he has to choose an integer a and subtract it from n such that:
    
    1 ≤ a ≤ n.
    If it's Mahmoud's turn, a has to be even, but if it's Ehab's turn, a has to be odd.
    If the current player can't choose any number satisfying the conditions, he loses. Can you determine the winner if they both play optimally?
    
    Input
    The only line contains an integer n (1 ≤ n ≤ 109), the number at the beginning of the game.
    
    Output
    Output "Mahmoud" (without quotes) if Mahmoud wins and "Ehab" (without quotes) otherwise.
    
    Examples
    inputCopy
    1
    outputCopy
    Ehab
    inputCopy
    2
    outputCopy
    Mahmoud
    Note
    In the first sample, Mahmoud can't choose any integer a initially because there is no positive even integer less than or equal to 1 so Ehab wins.
    
    In the second sample, Mahmoud has to choose a = 2 and subtract it from n. It's Ehab's turn and n = 0. There is no positive odd integer less than or equal to 0 so Mahmoud wins.

---
+++ 959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,40 @@
+Output Status: 
+
+By Rajakunalpandit, contest: Codeforces Round #473 (Div. 2), problem: (A) Mahmoud and Ehab and the even-odd game, Accepted
+
+
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <cctype>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+
+int32_t main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    ll n;
+    cin>> n;
+    
+    if(n%2 == 0){
+        cout<< "Mahmoud" << endl;
+    }else{
+        cout<< "Ehab" << endl;
+    }
+
+    
+	return 0;
+}
+
+
+
