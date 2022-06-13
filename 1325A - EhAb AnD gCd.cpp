9e7c0d7fc6f3dc742bd6a6d27a47ae4a0071c379
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-06-13 16:47:42 +0530

    Create 1325A - EhAb AnD gCd.cpp
    Output Status : 
    
    By Rajakunalpandit, contest: Codeforces Round #628 (Div. 2), problem: (A) EhAb AnD gCd, Accepted
    
    You are given a positive integer x. Find any such 2 positive integers a and b such that GCD(a,b)+LCM(a,b)=x.
    
    As a reminder, GCD(a,b) is the greatest integer that divides both a and b. Similarly, LCM(a,b) is the smallest integer such that both a and b divide it.
    
    It's guaranteed that the solution always exists. If there are several such pairs (a,b), you can output any of them.
    
    Input
    The first line contains a single integer t (1≤t≤100)  — the number of testcases.
    
    Each testcase consists of one line containing a single integer, x (2≤x≤109).
    
    Output
    For each testcase, output a pair of positive integers a and b (1≤a,b≤109) such that GCD(a,b)+LCM(a,b)=x. It's guaranteed that the solution always exists. If there are several such pairs (a,b), you can output any of them.
    
    Example
    inputCopy
    2
    2
    14
    outputCopy
    1 1
    6 4
    Note
    In the first testcase of the sample, GCD(1,1)+LCM(1,1)=1+1=2.
    
    In the second testcase of the sample, GCD(6,4)+LCM(6,4)=2+12=14

---
+++ 1325A - EhAb AnD gCd.cpp
@@ -0,0 +1,55 @@
+Output Status : 
+
+By Rajakunalpandit, contest: Codeforces Round #628 (Div. 2), problem: (A) EhAb AnD gCd, Accepted
+
+
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <cctype>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+
+// ll gcd(ll a,ll b){
+//     if(b == 0){
+//         return a;
+//     }
+//     return (b,a%b);
+// }
+
+// ll lcm (ll a,ll b){
+//     ll ans = gcd(a,b)/(a*b);
+// }
+
+
+int32_t main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    ll t;
+    cin>> t;
+    
+    while(t--){
+        
+        ll x;
+        cin>> x;
+        
+       cout<< 1 << " " << x-1 << endl;
+        
+    }
+
+    
+	return 0;
+}
+
+
+
