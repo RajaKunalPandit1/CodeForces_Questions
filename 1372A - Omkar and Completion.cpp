4c9ddfef2b89cadc0e2b3b2fda8d03e0da6d9645
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-07-10 16:37:37 +0530

    Create 1372A - Omkar and Completion.cpp
    Output Status : 
    
    By Rajakunalpandit, contest: Codeforces Round #655 (Div. 2), problem: (A) Omkar and Completion, Accepted
    
    You have been blessed as a child of Omkar. To express your gratitude, please solve this problem for Omkar!
    
    An array a of length n is called complete if all elements are positive and don't exceed 1000, and for all indices x,y,z (1≤x,y,z≤n), ax+ay≠az (not necessarily distinct).
    
    You are given one integer n. Please find any complete array of length n. It is guaranteed that under given constraints such array exists.
    
    Input
    Each test contains multiple test cases. The first line contains t (1≤t≤1000)  — the number of test cases. Description of the test cases follows.
    
    The only line of each test case contains one integer n (1≤n≤1000).
    
    It is guaranteed that the sum of n over all test cases does not exceed 1000.
    
    Output
    For each test case, print a complete array on a single line. All elements have to be integers between 1 and 1000 and for all indices x,y,z (1≤x,y,z≤n) (not necessarily distinct), ax+ay≠az must hold.
    
    If multiple solutions exist, you may print any.
    
    Example
    inputCopy
    2
    5
    4
    outputCopy
    1 5 3 77 12
    384 384 44 44
    Note
    It can be shown that the outputs above are valid for each test case. For example, 44+44≠384.
    
    Below are some examples of arrays that are NOT complete for the 1st test case:
    
    [1,2,3,4,5]
    
    Notice that a1+a2=a3.
    
    [1,3000,1,300,1]
    
    Notice that a2=3000>1000.

---
+++ 1372A - Omkar and Completion.cpp
@@ -0,0 +1,46 @@
+Output Status : 
+
+By Rajakunalpandit, contest: Codeforces Round #655 (Div. 2), problem: (A) Omkar and Completion, Accepted
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <cctype>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+
+int32_t main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    ll t;
+    cin>> t;
+    
+    while(t--){
+        
+        ll n;
+        cin>> n;
+        
+        for(int i=0;i<n;i++){
+            cout << 1 << " ";
+        }
+        cout<< endl;
+    }
+
+	return 0;
+}
+
+
+
+
+
