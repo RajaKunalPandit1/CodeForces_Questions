5fb6d7564d89ba15e86f5dd52ea508dd4e233171
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2022-09-09 23:20:01 +0530

    Create 1549A - Gregor and Cryptography.cpp
    Output Status : 
    
    By Rajakunalpandit, contest: Codeforces Round #736 (Div. 2), problem: (A) Gregor and Cryptography, Accepted
    
    Gregor is learning about RSA cryptography, and although he doesn't understand how RSA works, he is now fascinated with prime numbers and factoring them.
    
    Gregor's favorite prime number is P. Gregor wants to find two bases of P. Formally, Gregor is looking for two integers a and b which satisfy both of the following properties.
    
    Pmoda=Pmodb, where xmody denotes the remainder when x is divided by y, and
    2≤a<b≤P.
    Help Gregor find two bases of his favorite prime number!
    
    Input
    Each test contains multiple test cases. The first line contains the number of test cases t (1≤t≤1000).
    
    Each subsequent line contains the integer P (5≤P≤109), with P guaranteed to be prime.
    
    Output
    Your output should consist of t lines. Each line should consist of two integers a and b (2≤a<b≤P). If there are multiple possible solutions, print any.
    
    Example
    inputCopy
    2
    17
    5
    outputCopy
    3 5
    2 4
    Note
    The first query is P=17. a=3 and b=5 are valid bases in this case, because 17mod3=17mod5=2. There are other pairs which work as well.
    
    In the second query, with P=5, the only solution is a=2 and b=4.

---
+++ 1549A - Gregor and Cryptography.cpp
@@ -0,0 +1,45 @@
+Output Status : 
+
+By Rajakunalpandit, contest: Codeforces Round #736 (Div. 2), problem: (A) Gregor and Cryptography, Accepted
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_set>
+#include <cctype>
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+
+int32_t main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+    
+    ll t;
+    cin >> t;
+    
+    while(t--){
+        
+        ll p;
+        cin >> p;
+       
+        cout<< (p-1)/2 << " " << (p-1) << endl; 
+       
+    }
+    
+    
+
+	return 0;
+}
+
+
+
+
+
